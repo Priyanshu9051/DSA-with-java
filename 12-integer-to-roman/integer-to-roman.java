24d7a348ea75e95f72113787096dafff551574ae
Priyanshu sagar <priyanshusagar740@gmail.com>
2026-07-19 21:29:29 +0530

    Time: 3 ms (99.88%) | Memory: 46.2 MB (78.31%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.java
+++ 12-integer-to-roman/integer-to-roman.java
@@ -1,88 +1,28 @@
 class Solution {
     public String intToRoman(int num) {
 
-        Stack<String> st = new Stack<>();
-        int c = 0;
+        int[] values = {
+            1000, 900, 500, 400,
+            100, 90, 50, 40,
+            10, 9, 5, 4, 1
+        };
 
-        while (num != 0) {
+        String[] symbols = {
+            "M", "CM", "D", "CD",
+            "C", "XC", "L", "XL",
+            "X", "IX", "V", "IV", "I"
+        };
 
-            int a = num % 10;
-            num = num / 10;
+        StringBuilder ans = new StringBuilder();
 
-            if (a == 0) {
-                c++;
-            } 
-            else {
-                if (c == 0) {
-                    st.push(check(a));
-                    c++;
-                }
+        for (int i = 0; i < values.length; i++) {
 
-                else if (c == 1) {
-                    int b = a * 10;
-                    st.push(check(b));
-                    c++;
-                }
-
-                else if (c == 2) {
-                    int d = a * 100;
-                    st.push(check(d));
-                    c++;
-                }
-
-                else if (c == 3) {
-                    int e = a * 1000;
-                    st.push(check(e));
-                    c++;
-                }
+            while (num >= values[i]) {
+                ans.append(symbols[i]);
+                num -= values[i];
             }
         }
 
-        String s = "";
-
-        while (!st.isEmpty()) {
-            s = s + st.pop();
-        }
-
-        return s;
-    }
-
-    String check(int k) {
-
-        if (k == 1) return "I";
-        else if (k == 2) return "II";
-        else if (k == 3) return "III";
-        else if (k == 4) return "IV";
-        else if (k == 5) return "V";
-        else if (k == 6) return "VI";
-        else if (k == 7) return "VII";
-        else if (k == 8) return "VIII";
-        else if (k == 9) return "IX";
-
-        else if (k == 10) return "X";
-        else if (k == 20) return "XX";
-        else if (k == 30) return "XXX";
-        else if (k == 40) return "XL";
-        else if (k == 50) return "L";
-        else if (k == 60) return "LX";
-        else if (k == 70) return "LXX";
-        else if (k == 80) return "LXXX";
-        else if (k == 90) return "XC";
-
-        else if (k == 100) return "C";
-        else if (k == 200) return "CC";
-        else if (k == 300) return "CCC";
-        else if (k == 400) return "CD";
-        else if (k == 500) return "D";
-        else if (k == 600) return "DC";
-        else if (k == 700) return "DCC";
-        else if (k == 800) return "DCCC";
-        else if (k == 900) return "CM";
-
-        else if (k == 1000) return "M";
-        else if (k == 2000) return "MM";
-        else if (k == 3000) return "MMM";
-
-        return "";
+        return ans.toString();
     }
 }
