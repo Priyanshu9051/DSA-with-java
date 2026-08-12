174efc5833fb1eb57d122f6cd8972f19fed9281e
Priyanshu sagar <priyanshusagar740@gmail.com>
2026-08-12 21:39:19 +0530

    Time: 481 ms (5.23%) | Memory: 273.2 MB (5.80%) - LeetSync

---
+++ 187-repeated-dna-sequences/repeated-dna-sequences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public List<String> findRepeatedDnaSequences(String s) {
+        Set<String> set1=new HashSet<>();
+        Set<String> set2=new HashSet<>();
+        for(int i=0;i<=s.length()-10;i++){
+                String dna=s.substring(i,i+10);
+            if(set1.contains(dna)){
+                set2.add(dna);
+
+            }
+            
+                set1.add(dna);
+            
+
+        }
+        List<String> ans=new ArrayList<>(set2);
+        return ans;
+
+
+        
+    }
+}
